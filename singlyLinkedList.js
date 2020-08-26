020529cc945be0f9c7efd1ffd81234f01a652fb4
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-08-25 21:48:04 -0400

    implemented shift and shift methods

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -42,8 +42,32 @@ class SinglyLinkedList {
     }
     return current;
   }
+  shift() {
+    if (!this.head) return undefined;
+    var currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
-// initializing interface
+// initializing an interface
 let list = new SinglyLinkedList()
-list.push('HELLO')
+list.push('hello')
+list.push('goodbye')
+list.push('!')
