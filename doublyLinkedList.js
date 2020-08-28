030c647d6c434dc4357fa1d11b81d8543c31e26f
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-08-27 21:08:02 -0400

    implemented shift and unshift

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -40,4 +40,36 @@ class DoublyLinkedList {
     this.length--;
     return poppedNode
   }
+  shift() {
+    if (this.length === 0) return undefined;
+    var oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
+
+let list = new DoublyLinkedList()
+list.push("Harry")
+list.push("Ron")
+list.push("Hermione")
