a5c1afe0bf88cd766098f91ec77d7803a328c5a4
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-08-30 14:53:00 -0400

    Implemented a Stack

--- stacks.js
+++ stacks.js
@@ -1,3 +1,37 @@
-// implementation of a Stack with an array
+// implementation of a Stack with a Singly Linked List
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-// implementation of a Stack with a linked list
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  push(val) {
+    let newNode = new Node(val)
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+  pop() {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
