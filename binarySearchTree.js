ac080932b56abd6349c2abe2a9dc3382b134fe8f
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-08-31 21:40:58 -0400

    Added Contains/Find method

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -38,4 +38,18 @@ class BinarySearchTree {
       }
     }
   }
+  contains(value) {
+    if (this.root === null) return false;
+    let current = this.root, found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
 }
